b77c1a05bb6c85bd8b35dd8f6f1f6c9b49b572d9
AceBurgundy <Samadriansabalo99@gmail.com>
2022-09-03 21:27:56 +0800

    created app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from Engine import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
+
+    # if you are seeing this problem;
+    # Error: Could not locate a Flask application. You did not provide the "FLASK_APP" environment variable, and a "wsgi.py" or "app.py" module was not found in the current directory.
+
+    # run this and try again:    $env:FLASK_APP="run.py"
