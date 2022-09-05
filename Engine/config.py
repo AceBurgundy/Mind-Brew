a7a832d63c80e6b6d0f328227d45945b757a75ae
AceBurgundy <Samadriansabalo99@gmail.com>
2022-09-05 14:38:33 +0800

    Updated config file.

--- Engine/config.py
+++ Engine/config.py
@@ -4,12 +4,12 @@ load_dotenv()
 
 
 class Config:
-        # Ensure templates are auto-reloaded
+    # Ensure templates are auto-reloaded
     TEMPLATES_AUTO_RELOAD = True
 
-    SECRET_KEY = 'completebutincomplete'
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = 'sqlite:///reviewer.db'
-    
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Configure session to use filesystem (instead of signed cookies)
