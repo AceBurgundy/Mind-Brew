b192374623a41d1a98e1088995dd12b2588bb38f
AceBurgundy <Samadriansabalo99@gmail.com>
2022-09-11 17:46:19 +0800

    Imported from collab.

---
+++ Engine/profile/static/profile/profile.js
@@ -0,0 +1,30 @@
+const profileSection = document.getElementById('profile-section')
+const profileCancelButton = document.querySelector("#return-to-previous");
+
+window.addEventListener("load", () => {
+    profileSection.classList.toggle("active")
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+    if ((document.querySelector(".error")) !== null) {
+        profileSection.style.transform = "translate(-50%, -50%)"
+        profileSection.style.transition = "-10ms"
+        profileSection.style.opacity = "1"
+    }
+})
+
+profileCancelButton.addEventListener('click', () => {
+    if (profileSection.classList.contains("active")) {
+        profileSection.classList.remove("active")
+        setTimeout(() => {
+            window.history.back();
+        }, 300);
+    } else {
+        profileSection.style.transition = "500ms"
+        profileSection.style.transform = "translate(-50%, -40%)"
+        profileSection.style.opacity = "0"
+        setTimeout(() => {
+            window.history.back();
+        }, 300);
+    }
+})
