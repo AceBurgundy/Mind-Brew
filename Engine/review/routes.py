65680642305080c7bb4eb32a4f146599ae50a940
AceBurgundy <Samadriansabalo99@gmail.com>
2022-09-11 17:47:55 +0800

    Added route to start review.

--- Engine/review/routes.py
+++ Engine/review/routes.py
@@ -1,3 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for, jsonify
+from Engine import db
+from Engine.models import Reviewer, Subject
 
-review = Blueprint('review', __name__, template_folder='templates/review', static_folder='static/review')
+review = Blueprint('review', __name__,
+                   template_folder='templates/review', static_folder='static/review')
+
+
+@review.get("/review/<int:current_subject_id>")
+def start(current_subject_id):
+
+    subject = db.session.get(Subject, current_subject_id)
+    if (subject.bought_test == True):
+        questions = Reviewer.query.filter_by(subject_id=current_subject_id)
+        return render_template('test.html', questions=questions)
+    else:
+        return jsonify(message="Haven't bought the test yet")
