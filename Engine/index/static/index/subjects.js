f01a22cddfb62ad572feeca0ee040e5d521e7a36
AceBurgundy <Samadriansabalo99@gmail.com>
2022-09-11 17:43:05 +0800

    Added script for animating shopping cart and ajax to message admin for buying reviewer.

---
+++ Engine/index/static/index/subjects.js
@@ -0,0 +1,27 @@
+import { makeToastNotification } from "/static/helper.js"
+
+document.querySelectorAll(".shopping-cart-line").forEach(shoppingCart => {
+    shoppingCart.addEventListener("mouseover", () => {
+        shoppingCart.children[3].style.transition = '300ms'
+        shoppingCart.children[3].setAttribute("fill", "#c4c3cd")
+    })
+    shoppingCart.addEventListener("mouseout", () => {
+        shoppingCart.children[3].setAttribute("fill", "none")
+    })
+})
+
+$(document).ready(function() {
+    $(".buy-button").on('click', function(event) {
+        makeToastNotification("Emailing Admin")
+        const link = $(this).attr('data-link')
+        event.preventDefault()
+        let request = $.get(
+            link, "json")
+        request.done(function(data) {
+            makeToastNotification(data.message)
+        })
+        request.fail(function() {
+            makeToastNotification("Failed to buy reviewer")
+        })
+    })
+})
