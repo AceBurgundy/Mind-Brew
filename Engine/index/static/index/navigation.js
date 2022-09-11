4831c4eaf74b02db393616ff43bff83c3252b1e3
AceBurgundy <Samadriansabalo99@gmail.com>
2022-09-11 17:41:46 +0800

    Added script to toggle profile dropdown.

---
+++ Engine/index/static/index/navigation.js
@@ -0,0 +1,6 @@
+let profileDropdownButton = document.querySelector(".profile-dropdown-button");
+
+profileDropdownButton.addEventListener("click", () => {
+
+    profileDropdownButton.classList.toggle("active");
+})
