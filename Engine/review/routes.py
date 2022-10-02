278132f37625a710114b760c902da0b6b4df1e5a
AceBurgundy <Samadriansabalo99@gmail.com>
2022-10-02 18:28:53 +0800

    changed subject to reviewer.

--- Engine/review/routes.py
+++ Engine/review/routes.py
@@ -1,17 +1,14 @@
 from flask import Blueprint, render_template, redirect, url_for, jsonify
 from Engine import db
-from Engine.models import Reviewer, Subject
+from Engine.models import Test, Reviewer
 
 review = Blueprint('review', __name__,
                    template_folder='templates/review', static_folder='static/review')
 
 
-@review.get("/start/<int:current_subject_id>")
-def start(current_subject_id):
+@review.get("/start/<int:current_reviewer_id>")
+def start(current_reviewer_id):
 
-    subject = db.session.get(Subject, current_subject_id)
-    if (subject.bought_test == True):
-        questions = Reviewer.query.filter_by(subject_id=current_subject_id)
-        return render_template('test.html', questions=questions)
-    else:
-        return jsonify(message="Haven't bought the test yet")
+    questions = Test.query.filter_by(reviewer_id=current_reviewer_id)
+
+    return render_template('test.html', questions=questions)
