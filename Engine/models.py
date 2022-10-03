a531b7f713c5c5e790b8a70d51fe3db67e46963c
AceBurgundy <Samadriansabalo99@gmail.com>
2022-10-03 08:56:22 +0800

    Added table name.

--- Engine/models.py
+++ Engine/models.py
@@ -1,9 +1,6 @@
-import json
-from time import time
 from datetime import datetime
 from Engine import db, login_manager
 from flask_login import UserMixin
-from flask_admin.contrib.sqla import ModelView
 
 """
 The function below is a callback used to reload the user object from the user ID stored in the session.
@@ -16,18 +13,19 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-attempts = db.Table(
-    db.Column("id", db.Integer, primary_key=True, nullable=False),
-    db.Column("reviewer_id", db.Integer, db.ForeignKey(
-        'reviewer.id'), nullable=False),
-    db.Column("user_id", db.Integer, db.ForeignKey(
-        'user.id'), nullable=False),
-    db.Column("status", db.String(20), nullable=False),
-    db.Column("score", db.Integer, nullable=False, default=0),
-    db.Column("start_time", db.DateTime(),
-              default=datetime.now, nullable=False),
-    db.Column("end_time", db.DateTime(), default=datetime.now),
-)
+attempts = db.Table("attempts",
+                    db.Column("id", db.Integer,
+                              primary_key=True, nullable=False),
+                    db.Column("reviewer_id", db.Integer, db.ForeignKey(
+                        'reviewer.id'), nullable=False),
+                    db.Column("user_id", db.Integer, db.ForeignKey(
+                        'user.id'), nullable=False),
+                    db.Column("status", db.String(20), nullable=False),
+                    db.Column("score", db.Integer, nullable=False, default=0),
+                    db.Column("start_time", db.DateTime(),
+                              default=datetime.now, nullable=False),
+                    db.Column("end_time", db.DateTime(), default=datetime.now),
+                    )
 
 
 class User(db.Model, UserMixin):
@@ -53,7 +51,7 @@ class User(db.Model, UserMixin):
         'Message', backref='received_messages', lazy=True)
 
     attempts = db.relationship(
-        'Project', secondary=attempts, backref='taker')
+        'Reviewer', secondary=attempts, backref='attempts')
     # # this is not a column so we wont see a projects column in the User database. Instead,
     # # it runs a additional querry in the backrground to match the projects that the user has created
 
@@ -83,6 +81,8 @@ class Reviewer(db.Model, UserMixin):
     image = db.Column(
         db.String(100), nullable=False, default='reviewer.jpg')
     score = db.Column(db.Integer, nullable=False, default=0)
+    details = db.Column(
+        db.String(200), default="A new reviewer", nullable=False)
     availablility = db.Column(db.Boolean, nullable=False, default=True)
     students = db.Column(db.Integer, db.ForeignKey('user.id'))
     questions = db.relationship(
@@ -105,10 +105,10 @@ class Question(db.Model, UserMixin):
         'Choice', backref='question', lazy=True, passive_deletes=True)
 
     answers = db.relationship(
-        'Choice', backref='question', lazy=True, uselist=False, passive_deletes=True)
+        'Answer', backref='question', lazy=True, uselist=False, passive_deletes=True)
 
     reviewer_id = db.Column(db.Integer, db.ForeignKey(
-        'reviewer.id'), nullable=False, ondelete="CASCADE")
+        'reviewer.id', ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         return f"Question('{self.question}')"
@@ -123,7 +123,7 @@ class Choice(db.Model, UserMixin):
     choice = db.Column(db.String(100), nullable=False)
 
     question_id = db.Column(db.Integer, db.ForeignKey(
-        'question.id'), nullable=False, ondelete="CASCADE")
+        'question.id', ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         return f"Choice('{self.choice}')"
@@ -139,7 +139,7 @@ class Answer(db.Model, UserMixin):
     answer = db.Column(db.String(100), nullable=False)
 
     question_id = db.Column(db.Integer, db.ForeignKey(
-        'question.id'), nullable=False, ondelete="CASCADE")
+        'question.id', ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         return f"Answer('{self.answer}')"
