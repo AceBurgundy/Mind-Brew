574f34086173a8365816c6401f2e96d81369a133
AceBurgundy <Samadriansabalo99@gmail.com>
2022-10-07 18:16:25 +0800

    added user-reviewers table.

--- Engine/models.py
+++ Engine/models.py
@@ -27,6 +27,17 @@ attempts = db.Table("attempts",
                     db.Column("end_time", db.DateTime(), default=datetime.now),
                     )
 
+user_reviewers = db.Table("user-reviewers",
+                          db.Column("id", db.Integer,
+                                    primary_key=True, nullable=False),
+                          db.Column("reviewer_id", db.Integer, db.ForeignKey(
+                              'reviewer.id'), nullable=False),
+                          db.Column("user_id", db.Integer, db.ForeignKey(
+                              'user.id'), nullable=False),
+                          db.Column("bought_date",  db.DateTime(),
+                                    default=datetime.now, nullable=False)
+                          )
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -45,10 +56,8 @@ class User(db.Model, UserMixin):
     last_online = db.Column(
         db.DateTime(), default=datetime.now, onupdate=datetime.now, nullable=False)
 
-    reviewers = db.relationship('Reviewer', backref='owned_by', lazy=True)
-
-    messages = db.relationship(
-        'Message', backref='received_messages', lazy=True)
+    reviewers = db.relationship(
+        'Reviewer', secondary=user_reviewers, backref='owners', lazy=True)
 
     attempts = db.relationship(
         'Reviewer', secondary=attempts, backref='attempts')
@@ -56,7 +65,7 @@ class User(db.Model, UserMixin):
     # # it runs a additional querry in the backrground to match the projects that the user has created
 
     def __repr__(self):
-        return f"User('{self.username}','{self.reviewers}','{self.messages}') "
+        return f"User('{self.username}','{self.reviewers}') "
 
 
 class Message(db.Model, UserMixin):
@@ -64,12 +73,14 @@ class Message(db.Model, UserMixin):
     sender_id = db.Column(db.Integer, nullable=False)
     recipient_id = db.Column(
         db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message_type = db.Column(db.String(60), nullable=False)
     body = db.Column(db.Text, nullable=False)
+    reviewer_id = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime, index=True,
                           default=datetime.utcnow, nullable=False)
 
     def __repr__(self):
-        return f"Message('{self.sender_id}','{self.recipient_id}','{self.body}')"
+        return f"Message('{self.id}','{self.recipient_id}','{self.body}')"
 
 
 class Reviewer(db.Model, UserMixin):
@@ -80,38 +91,40 @@ class Reviewer(db.Model, UserMixin):
     author = db.Column(db.String(80), nullable=False)
     image = db.Column(
         db.String(100), nullable=False, default='reviewer.jpg')
-    score = db.Column(db.Integer, nullable=False, default=0)
+    expected_score = db.Column(db.Integer, nullable=False, default=0)
     details = db.Column(
         db.String(200), default="A new reviewer", nullable=False)
-    availablility = db.Column(db.Boolean, nullable=False, default=True)
-    students = db.Column(db.Integer, db.ForeignKey('user.id'))
+    availability = db.Column(db.Boolean, nullable=False, default=True)
     questions = db.relationship(
         'Question', backref='reviewer', lazy=True, passive_deletes=True)
 
     def __repr__(self):
-        return f"Reviewer('{self.name}','{self.author}') "
+        return f"Reviewer('{self.id}', '{self.name}','{self.author}') "
 
 
 class Question(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
     creation_date = db.Column(
-        db.DateTime(), default=datetime.now, nullable=False)
+        db.DateTime(), default=datetime.now)
     updated_date = db.Column(
-        db.DateTime(), default=datetime.now, onupdate=datetime.now, nullable=False)
+        db.DateTime(), default=datetime.now, onupdate=datetime.now)
 
     question = db.Column(db.String(1000), nullable=False)
+
+    type = db.Column(db.String(20), nullable=False)
+    # multiple choice, identification, enumeration.
 
     choices = db.relationship(
         'Choice', backref='question', lazy=True, passive_deletes=True)
 
-    answers = db.relationship(
+    answer = db.relationship(
         'Answer', backref='question', lazy=True, uselist=False, passive_deletes=True)
 
     reviewer_id = db.Column(db.Integer, db.ForeignKey(
-        'reviewer.id', ondelete="CASCADE"), nullable=False)
+        'reviewer.id', ondelete="CASCADE"))
 
     def __repr__(self):
-        return f"Question('{self.question}')"
+        return f"Question('{self.question}', '{self.reviewer_id}', '{self.type}','{self.choices}','{self.answer}')"
 
 
 class Choice(db.Model, UserMixin):
