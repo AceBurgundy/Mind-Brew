aec9e4b816272e2de5ad9a2077e74d40d4262a16
AceBurgundy <Samadriansabalo99@gmail.com>
2023-01-20 14:03:14 +0800

    Added view for Subject table.

--- Engine/admin/routes.py
+++ Engine/admin/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, request, url_for
 from Engine import main_admin, db
-from Engine.models import Message, User, Reviewer, Question, Choice, Answer
+from Engine.models import Message, User, Reviewer, Question, Subject, Choice, Answer
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 import os
@@ -10,17 +10,21 @@ author = Blueprint('author', __name__,
 
 
 class myModelView(ModelView):
-    def is_accesible():
-        if current_user.username == os.getenv("ADMIN") and current_user.is_authenticated:
-            return current_user.is_authenticated
+
+    def is_accessible(self):
+        if current_user.is_authenticated and current_user == User.query.filter_by(username=os.getenv("ADMIN")).first():
+            return True
+        return False
 
     def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('user.login'))
+        # redirect to login page if user doesn't have access
+        return redirect(url_for('app-user.login'))
 
 
 main_admin.add_view(myModelView(User, db.session))
 main_admin.add_view(myModelView(Message, db.session))
 main_admin.add_view(myModelView(Reviewer, db.session))
+main_admin.add_view(myModelView(Subject, db.session))
 main_admin.add_view(myModelView(Question, db.session))
 main_admin.add_view(myModelView(Choice, db.session))
 main_admin.add_view(myModelView(Answer, db.session))
